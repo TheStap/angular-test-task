ddd102229c74f91365ba843f689ba38927af5a8f
Artem Malikov <ineveraskedforthis@bk.ru>
2018-09-11 22:57:13 +0400

    environment prod config, build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+    production: true,
+    apiBaseUrl: 'https://api.stackexchange.com/2.2/',
+    site: 'stackoverflow',
+    clientId: 13060,
+    key: 'yVx2*fXozwIGv4FUR9frEA((',
+    channelUrl: 'http://localhost/blankPage',
 };
